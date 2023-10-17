511a356b54f121c96d234d7c5d145dc6bf839f0b
Liviu <liviumocanu20009@gmail.com>
2023-10-17 21:16:47 +0300

    Add Dockerfiles

--- gateway/config/config.py
+++ gateway/config/config.py
@@ -1,17 +1,18 @@
 from pydantic.v1 import BaseSettings
+import os
 
 
 class Config(BaseSettings):
-    REDIS_HOST: str = "localhost"
+    AWS_ACCESS_KEY: str = os.environ.get("AWS_ACCESS_KEY", "default_value")
+    AWS_SECRET_KEY: str = os.environ.get("AWS_SECRET_KEY", "default_value")
+    REDIS_HOST: str = "redis-service"
     REDIS_PORT: int = 6379
-    USER_SERVICE_HOST: str = "localhost"
+    USER_SERVICE_HOST: str = "user-service"
     USER_SERVICE_PORT: int = 50051
-    TRACK_SERVICE_HOST: str = "localhost"
+    TRACK_SERVICE_HOST: str = "track-service"
     TRACK_SERVICE_PORT: int = 50052
-    PLAYBACK_SERVICE_HOST: str = "localhost"
+    PLAYBACK_SERVICE_HOST: str = "playback-service"
     PLAYBACK_SERVICE_PORT: int = 50053
-    AWS_ACCESS_KEY: str = "AKIAS7M73W4WKXNEBD7C"
-    AWS_SECRET_KEY: str = "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"
     BUCKET_NAME: str = "tracks-bucket-pad"
     TIMEOUT: int = 5
     CACHE_INVALIDATION_TIME: int = 10
